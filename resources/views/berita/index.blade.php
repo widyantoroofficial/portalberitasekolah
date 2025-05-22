a2a1cf924b114c31f63b97519abc82f7c686f4c5
adindaagustinmargarets <mikozua45@gmail.com>
2025-05-22 18:53:41 +0700

    Update index.blade.php

--- resources/views/berita/index.blade.php
+++ resources/views/berita/index.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h2 class="text-center">Berita Terbaru</h2>
-        <div class="row">
-            @foreach($beritas as $berita)
-                <div class="col-md-4 mb-4">
-                    <div class="card">
-                        <img src="{{ asset('storage/' . $berita->image_path) }}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $berita->title }}</h5>
-                            <p class="card-text">{{ \Str::limit($berita->description, 100) }}</p>
-                            <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
-                        </div>
-                    </div>
+<div class="container">
+    <h2 class="text-center">Berita Terbaruu</h2>
+    <div class="row">
+        @foreach($beritas as $berita)
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="{{ asset('storage/' . $berita->image_path) }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $berita->title }}</h5>
+                    <p class="card-text">{{ \Str::limit($berita->description, 100) }}</p>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
                 </div>
-            @endforeach
+            </div>
         </div>
+        @endforeach
     </div>
+</div>
 @endsection
